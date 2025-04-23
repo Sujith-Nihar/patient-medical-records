a40e6eb5322832b1535bd051c10f1ef1ca907f0b
Sujith-Nihar <sujitheuphoria3@gmail.com>
2025-04-22 20:52:53 -0500

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  base:"/patient-medical-records",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
+  base:"/patient-medical-records"
 }));
